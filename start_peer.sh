c15df66bb97cf7041ac95b878a298030107a5f17
Oleksandr Borisenko <o.borisenko%40external.oberthur.com>
2017-08-18 16:07:57 +0300

    modified:   fabric_k8s_peer1_rc.yaml 	modified:   start_peer.sh

--- start_peer.sh
+++ start_peer.sh
@@ -1,3 +1,6 @@
-tar -xvf $FABRIC_CFG_PATH/peer0.org1.tar -C $FABRIC_CFG_PATH/
+tar -xvf /etc/fabric_crypto/peer0.org1.tar -C $FABRIC_CFG_PATH/
+chown -R root.root $FABRIC_CFG_PATH/
+rm -f /etc/hyperledger/fabric/msp/config.yaml
 sleep 3
 peer node start
+sleep 400
